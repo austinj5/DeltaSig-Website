0d3bbaf343cf5e299f6ada9c19daf4e0ca699eaa
austinj5 <112716279+austinj5@users.noreply.github.com>
2024-11-14 15:19:08 -0500

    added header home and about components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    AboutComponent,
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeaderComponent } from './header/header.component';
+
+export const routes: Routes = [
+    { path: '', component: HomeComponent },         // Default home page route
+    { path: 'about', component: AboutComponent },   // About Us page route
+];
+
